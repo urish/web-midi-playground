eb02cf71c45e6e79bc8b22991616782e5e4a14e7
Uri Shaked <uri@urishaked.com>
2018-11-08 17:13:23 +0000

    feat: switch editor to TypeScript, load Web MIDI types, run button
    also make font size larger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MonacoEditorModule } from 'ngx-monaco-editor';
-import { MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor';
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,18 +17,25 @@ import { NuggetListComponent } from './nugget-list/nugget-list.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { NuggetEditorComponent } from './nugget-editor/nugget-editor.component';
 import { CommonModule } from '@angular/common';
+import { webMidiTypes } from './web-midi-types';
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  onMonacoLoad: () => {
+    // This makes Web MIDI APIs available in the editor
+    monaco.languages.typescript.typescriptDefaults.addExtraLib(
+      webMidiTypes,
+      'webmidi.d.ts'
+    );
+  }
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NuggetListComponent,
-    NuggetEditorComponent
-  ],
+  declarations: [AppComponent, NuggetListComponent, NuggetEditorComponent],
   imports: [
     BrowserModule,
     CommonModule,
     AppRoutingModule,
-    MonacoEditorModule.forRoot(),
+    MonacoEditorModule.forRoot(monacoConfig),
     FormsModule,
     BrowserAnimationsModule,
     MatToolbarModule,
@@ -30,9 +43,9 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     LayoutModule,
     MatSidenavModule,
-    MatListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/nugget-editor/nugget-editor.component.ts
+++ src/app/nugget-editor/nugget-editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LanguageService } from 'typescript';
 import { codeTemplate } from '../code-template';
+
 @Component({
   selector: 'app-nugget-editor',
   templateUrl: './nugget-editor.component.html',
@@ -8,11 +10,20 @@ import { codeTemplate } from '../code-template';
 export class NuggetEditorComponent implements OnInit {
   readonly editorOptions = {
     theme: 'vs-dark',
-    language: 'javascript'
+    language: 'typescript',
+    fontSize: 20
   };
   code = codeTemplate;
 
   constructor() {}
 
   ngOnInit() {}
+
+  async runCode() {
+    const model = monaco.editor.getModels()[0];
+    const worker = await monaco.languages.typescript.getTypeScriptWorker();
+    const client: LanguageService = await worker(model.uri);
+    const result = await client.getEmitOutput(model.uri.toString());
+    alert(result.outputFiles[0].text);
+  }
 }
